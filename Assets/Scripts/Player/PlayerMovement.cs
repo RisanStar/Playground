71df2b355f9a6561de7dd0b69332dab51fa53320
RisanStar <nBuckley2023@elam.co.uk>
2024-09-27 12:46:16 +0100

    Started roll timer

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -36,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float coyoteTime;
     [SerializeField] private float jumpBuffTime;
 
+    [Header("Roll")]
+    private float rollTime;
     private enum AnimState {idle, running, jumping, rolling}
     private void Awake()
     {
@@ -74,6 +76,19 @@ public class PlayerMovement : MonoBehaviour
             if (jumpBuffTime < 0)
             {
                 jumpBuffTime = 0;
+            }
+        }
+
+        if(Keyboard.current.dKey.isPressed || Keyboard.current.aKey.isPressed)
+        {
+            rollTime = .4f;
+        }
+        else
+        {
+            rollTime -= Time.deltaTime;
+            if (rollTime < 0)
+            {
+                rollTime = 0;
             }
         }
 
